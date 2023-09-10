1e3acfa15fd78c70e133b0f5bc6cf8eecdbb979f
Matthieu Tisserand <matthieu.tisserand@polytechnique.org>
2023-09-11 00:09:33 +0200

    Load channels in a DataFrame

--- src/IPTVChannelizer.jl
+++ src/IPTVChannelizer.jl
@@ -5,11 +5,12 @@ export parse_playlist
 
 # Reading the original m3u file
 m3u_file = "./test/data/input/playlist.m3u"
-# channels = parse_playlist(m3u_file)
+channels = parse_playlist(m3u_file)
+# https://discourse.julialang.org/t/construct-dataframe-from-uneven-named-tuples/102970
 
-# using DataFrames
-# df = DataFrame(channels)
-# first(df,5)
+using DataFrames
+df = DataFrame(Tables.dictrowtable(channels))
+first(df,5)
 
 # m3u_output_file = "/home/jovyan/work/dev/julia/data/m3u/playlist.m3u"
 # df_fr = filter(row -> row.country == "FR", df)
